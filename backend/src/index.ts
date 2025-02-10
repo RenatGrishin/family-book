789ab970358fdf4f55ea35e3d466812ce6f723d6
r.grishin <r.grishin@dataduck.com>
2025-02-10 13:35:32 +0300

    add nodemon

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors"; // Для разрешения запросов с фронтенда
 import { Pool } from "pg"; // Подключаем библиотеку pg
 
-import { crateTableDateSettings } from "./tablets/date_settings";
+//import { crateTableDateSettings } from "./tablets/date_settings";
 
 const app = express();
 const port = 4000;
@@ -20,41 +20,41 @@ const pool = new Pool({
 });
 
 // // Маршрут для создания таблицы person
-app.post("/create-person-table", async (req: Request, res: Response) => {
-	try {
-		crateTableDateSettings(pool);
+// app.post("/create-person-table", async (req: Request, res: Response) => {
+// 	try {
+// 		crateTableDateSettings(pool);
 
-		res.status(200).send("Таблица person успешно создана!");
-	} catch (err) {
-		console.error("Ошибка при создании таблицы:", err);
-		res.status(500).send("Ошибка при создании таблицы");
-	}
-});
+// 		res.status(200).send("Таблица person успешно создана!");
+// 	} catch (err) {
+// 		console.error("Ошибка при создании таблицы:", err);
+// 		res.status(500).send("Ошибка при создании таблицы");
+// 	}
+// });
 
-// Маршрут для получения всех таблиц
-app.get("/tables", async (req: Request, res: Response) => {
-	try {
-		const client = await pool.connect();
+// // Маршрут для получения всех таблиц
+// app.get("/tables", async (req: Request, res: Response) => {
+// 	try {
+// 		const client = await pool.connect();
 
-		// Запрос для получения всех таблиц
-		const result = await client.query(`
-		SELECT tablename
-		FROM pg_tables
-		WHERE schemaname = 'public';
-	  `);
+// 		// Запрос для получения всех таблиц
+// 		const result = await client.query(`
+// 		SELECT tablename
+// 		FROM pg_tables
+// 		WHERE schemaname = 'public';
+// 	  `);
 
-		client.release();
+// 		client.release();
 
-		// Отправляем результат клиенту
-		res.status(200).json(result.rows);
-	} catch (err) {
-		console.error("Ошибка при получении таблиц:", err);
-		res.status(500).send("Ошибка при получении таблиц");
-	}
-});
+// 		// Отправляем результат клиенту
+// 		res.status(200).json(result.rows);
+// 	} catch (err) {
+// 		console.error("Ошибка при получении таблиц:", err);
+// 		res.status(500).send("Ошибка при получении таблиц");
+// 	}
+// });
 
 app.get("/", (req: Request, res: Response) => {
-	res.send("Hello from Express backend!");
+	res.send("Hello from Express backend!!!");
 });
 
 app.listen(port, () => {
