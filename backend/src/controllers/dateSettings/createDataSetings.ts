71c7b89dba4fe05e03a44590f2d3fb63a9f546f9
r.grishin <r.grishin@dataduck.com>
2025-02-17 10:17:58 +0300

    007B

--- backend/src/controllers/dateSettings/createDataSetings.ts
+++ backend/src/controllers/dateSettings/createDataSetings.ts
@@ -1,17 +1,9 @@
 import { Response, Request } from "express";
-import { pool } from "../../db/pool";
+import { createDataSettingsService } from "../../services/dataSettings/createDataSettingsService";
 
 export const createDataSetings = async (req: Request, res: Response) => {
 	try {
-		const query = `
-			CREATE TABLE date_settings (
-			id SERIAL PRIMARY KEY,
-			date_type VARCHAR(50) NOT NULL CHECK (date_type IN ('exact', 'approximate', 'before', 'after', 'between')),
-			start_date DATE NOT NULL,
-			end_date DATE
-			);
-	  	`;
-		await pool.query(query);
+		await createDataSettingsService();
 		res.status(201).json({
 			message: "Таблица date_settings успешно создана",
 		});
